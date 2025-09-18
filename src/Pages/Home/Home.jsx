4215ad9c91726c370e0b68c3494b32fa2b2aa4be
zack-das <izackdas@gmail.com>
2025-09-18 10:55:46 +0300

    Home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Home.css"
+
+function Home() {
+	return(
+		<section className="home-section">
+			<div className="hero">
+				<div className="hero-container">
+					<h1>Sweat.Smile. Repeat.</h1>
+				<p>Transform your body and mind.<br/> We're not just a gym; we're your launchpad to a stronger,<br/> healthier you. Get ready to crush your goals!</p>
+
+				<div className="hero-cta">
+					<button>Get Started</button>
+				</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Home;
