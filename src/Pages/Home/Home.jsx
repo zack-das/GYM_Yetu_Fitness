c3bb952d7ffd7e7c43586d3ec56bd2fca8ce612e
zack-das <izackdas@gmail.com>
2025-10-07 12:03:25 +0300

    Updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Home.css"
 import About from "../About/About"
-mport gymRoom from "../../assets/gym-room.jpg";
+mport gymRoom from "../../assets/GYMroom.jpg";
 
 function Home() {
 	return(
