743044d0763b9ce4102f08c43540eb55780bb8bf
zack-das <izackdas@gmail.com>
2025-10-07 12:37:12 +0300

    Updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,12 +7,7 @@ function Home() {
 	return(
 		<>
 		<section className="home-section">
-			<div className="hero"
-  style={{
-   backgroundImage: 'url("/gym-room.jpg")',
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  }}>
+			<div className="hero">
 				<div className="hero-container">
 					<h1>Sweat.Smile. Repeat.</h1>
 				<p>Transform your body and mind.<br/> We're not just a gym; we're your launchpad to a stronger,<br/> healthier you. Get ready to crush your goals!</p>
