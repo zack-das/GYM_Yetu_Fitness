c24fe83716cee1db7ccda115c430ec3315cb58a6
zack-das <izackdas@gmail.com>
2025-10-07 11:46:58 +0300

    Updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./Home.css"
 import About from "../About/About"
+import gymRoom from "../assets/GYM room.jpg";
 
 function Home() {
 	return(
 		<>
 		<section className="home-section">
-			<div className="hero">
+			<div className="hero"    className="hero"
+  style={{
+    backgroundImage: `url(${gymRoom})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}>
 				<div className="hero-container">
 					<h1>Sweat.Smile. Repeat.</h1>
 				<p>Transform your body and mind.<br/> We're not just a gym; we're your launchpad to a stronger,<br/> healthier you. Get ready to crush your goals!</p>
